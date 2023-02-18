d5f9295d914f00cb1650c57d2e37ca1222e1331e
ruhulfbr <ruhul11bd@gmail.com>
2023-02-19 01:57:50 +0600

    print draw entry points

--- hand-tracking-min.py
+++ hand-tracking-min.py
@@ -20,6 +20,16 @@ while True:
 
     if results.multi_hand_landmarks:
         for handLms in results.multi_hand_landmarks:
+            for id, landMK in enumerate(handLms.landmark):
+                # print(id, landMK)
+                imgHeight, imgWidth, imgC = img.shape
+                cx, cy = int(landMK.x * imgWidth), int(landMK.y * imgHeight)
+                print(id, cx, cy)
+                if id == 0:
+                    cv2.circle(img, (cx, cy), 10, (255, 0, 0), cv2.FILLED)
+                elif id==4:
+                    cv2.circle(img, (cx, cy), 10, (255, 0, 0), cv2.FILLED)
+
             mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
 
     #Get Frame per secoend
@@ -28,7 +38,7 @@ while True:
     pTime = cTime
 
 
-    #Display Text for Frame per secoend
+    #Display Text for Frame per secoend DOC link for openCv put text - https://www.geeksforgeeks.org/python-opencv-cv2-puttext-method/
     cv2.putText(img, str(int(fps)), (10,70), cv2.FONT_HERSHEY_PLAIN, 3, (255,255,0), 3)
 
     cv2.imshow("Image", img)
